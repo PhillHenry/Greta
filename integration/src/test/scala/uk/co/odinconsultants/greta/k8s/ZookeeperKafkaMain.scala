3084927fbc1da34ecc3e255e09d11c03475a79f5
Phillip Henry <PhillHenry@gmail.com>
2020-03-02 10:46:25 +0000

    Destroy client in afterAll.

--- integration/src/test/scala/uk/co/odinconsultants/greta/k8s/ZookeeperKafkaMain.scala
+++ integration/src/test/scala/uk/co/odinconsultants/greta/k8s/ZookeeperKafkaMain.scala
@@ -3,10 +3,10 @@ package uk.co.odinconsultants.greta.k8s
 import io.fabric8.kubernetes.api.model.DoneableService
 import io.fabric8.kubernetes.api.model.ServiceFluent.SpecNested
 import io.fabric8.kubernetes.client.DefaultKubernetesClient
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 import uk.co.odinconsultants.greta.k8s.Commands._
 
-class ZookeeperKafkaMain extends WordSpec with Matchers {
+class ZookeeperKafkaMain extends WordSpec with Matchers with BeforeAndAfterAll {
 
   val zookeeper: SpecPipe =
     withType(ClusterIP) andThen
@@ -24,16 +24,17 @@ class ZookeeperKafkaMain extends WordSpec with Matchers {
 
   val kafkaHeadless: SpecPipe = kafka andThen withType(ClusterIP)
 
-  val namespace = "phtest"
+  val namespace             = "phtest"
 
   val headlessZookeeperName = "ph-release-zookeeper-headless"
   val zookeeperName         = "ph-release-zookeeper"
   val kafkaName             = "ph-release-kafka"
   val kafkaHeadlessName     = "ph-release-kafka-headless"
 
+  val client                = new DefaultKubernetesClient()
+
   "Zookeeper and Kafka" should {
     "fire up" in {
-      val client    = new DefaultKubernetesClient()
       val namespaced: Namespaced = client.services.inNamespace(namespace)
 
       createNamespace(namespace)(client) // TODO make this FP
@@ -45,14 +46,20 @@ class ZookeeperKafkaMain extends WordSpec with Matchers {
         kafkaHeadless(serviceFrom(namespaced, kafkaHeadlessName))
       )
       services.map(_.endSpec().done())
-      val actualServices = listServices(client)
-      val serviceNames = actualServices.map(_.getMetadata.getName)
+      val actualServices  = listServices(client)
+      val serviceNames    = actualServices.map(_.getMetadata.getName)
 
       serviceNames should contain (headlessZookeeperName)
-
-      client.namespaces.withName(namespace).delete()
-      client.close()
+      serviceNames should contain (zookeeperName)
+      serviceNames should contain (kafkaName)
+      serviceNames should contain (kafkaHeadlessName)
     }
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    client.namespaces.withName(namespace).delete()
+    client.close()
   }
 
 }
